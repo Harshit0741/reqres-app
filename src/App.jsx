60ee5622c044cb3d9127b90d9109e6d1ca51f4eb
Harshit <harshitnag12@gmail.com>
2025-03-28 16:55:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import UsersList from "./pages/UsersList";
 import EditUser from "./pages/EditUser";
